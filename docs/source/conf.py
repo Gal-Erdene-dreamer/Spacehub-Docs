665b2d5f0899220258714edd1eba0471b7e26418
Gal-Erdene-dreamer <77901976+Gal-Erdene-dreamer@users.noreply.github.com>
2022-03-19 16:13:20 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,9 @@
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
 project = 'Spacehub'
 copyright = '2021, Graziella'
 author = 'Graziella'
