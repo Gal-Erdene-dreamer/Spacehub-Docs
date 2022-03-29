9a2a14f45eecd5ab6f25f40ae5afa8fcb15ec208
Gal-Erdene-dreamer <77901976+Gal-Erdene-dreamer@users.noreply.github.com>
2022-03-29 15:27:40 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,8 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
-
+html_theme = 'stanford_theme'
 #html_theme = 'groundwork'
-html_theme = 'nature'
+#html_theme = 'nature'
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
